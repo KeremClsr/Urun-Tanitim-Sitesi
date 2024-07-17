ad50c1f762d97e1d7282d2561853d51c585dd217
KeremClsr <118075185+KeremClsr@users.noreply.github.com>
2024-07-17 17:05:03 +0300

    Düzenleme
    Hata sayfaları ve bazı mesajlar yeniden düzenlendi.

--- Kırtasiyeci/microblog/app/errors.py
+++ Kırtasiyeci/microblog/app/errors.py
@@ -3,6 +3,10 @@ from app import db
 
 bp = Blueprint('errors', __name__)
 
+@bp.app_errorhandler(403)
+def forbidden_error(error):
+    return render_template('403.html'), 403
+
 @bp.app_errorhandler(404)
 def not_found_error(error):
     return render_template('404.html'), 404
